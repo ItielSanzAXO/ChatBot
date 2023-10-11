8885b9225fdf2f2fbb74e321627f48a31ad58a6f
ItielSanz /AXO <113627289+ItielSanzAXO@users.noreply.github.com>
2023-10-11 00:19:23 -0600

    ChatBot

---
+++ mi_chatbot/app.py
@@ -0,0 +1,36 @@
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+account_sid = 'AC4a72771d366606f20c83bb5d8fde1435'
+auth_token = 'e9d808d8714234d2d6eb9906ebaa6048'
+client = Client(account_sid, auth_token)
+
+def responder_mensaje(mensaje):
+    # Aquí puedes implementar la lógica del chatbot para procesar el mensaje del usuario y generar una respuesta.
+    if "Hola" in mensaje:
+        return "Hola, ¿en qué puedo ayudarte?"
+    elif "Resultados" in mensaje:
+        return "Por supuesto, ¿cuál es el número de estudio?"
+    elif "Ubicación" in mensaje:
+        return "La ubicación del paciente es en la sala 203."
+    elif "Otro lugar" in mensaje:
+        return "¿A cuál área del hospital te refieres?"
+
+def enviar_respuesta_twilio(respuesta, numero_destino):
+    response = MessagingResponse()
+    response.message(respuesta)
+
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=respuesta,
+        to=f'whatsapp:{numero_destino}'
+    )
+
+    return message.sid
+
+# Manejo de mensajes entrantes
+mensaje_usuario = "Hola"  # Simula un mensaje de usuario
+numero_destino = "5215536688304"  # Reemplaza con el número de destino correcto
+
+respuesta = responder_mensaje(mensaje_usuario)
+enviar_respuesta_twilio(respuesta, numero_destino)
